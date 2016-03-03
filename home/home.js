8bdd8fe9eaa88a97691de19862112d9d67e0f074
Jack Collins <BlueSkyFlyer2@gmail.com>
2016-03-03 08:41:08 -0700

    Added an Our Services section with card panels

--- home/home.js
+++ home/home.js
@@ -1,7 +1,18 @@
 var statelineApp = angular.module('statelineApp');
 
-statelineApp.controller('HomeController', [function() {
+statelineApp.controller('HomeController', ['$scope', function($scope) {
   console.log("Initialized the home controller");
   $('.slider').slider();
   $('.materialboxed').materialbox();
+
+  $scope.specialties = [
+    {name: 'Disc Herniation'},
+    {name: 'Sports, Auto, and Work Injuries'},
+    {name: 'Whiplash and Spinal Treatments'},
+    {name: 'Headaches and Migraines'},
+    {name: 'Neck Pain and Stiffness'},
+    {name: 'Back, Hip, and Leg Pain'},
+    {name: 'Nutritional and Herbal Medicine'},
+    {name: 'Se Habla Espanol'}
+  ]
 }]);
