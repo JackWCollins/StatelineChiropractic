b4eb71f5bb964b59fea966b3e8515d33eab8dcf5
Jack Collins <BlueSkyFlyer2@gmail.com>
2016-03-02 19:27:49 -0700

    AngularJS working with Materialize CSS

--- home/home.js
+++ home/home.js
@@ -2,4 +2,5 @@ var statelineApp = angular.module('statelineApp');
 
 statelineApp.controller('HomeController', [function() {
   console.log("Initialized the home controller");
+  $('.slider').slider();
 }]);
