37c93eb40e1b07e0e758e86b45cd662be2729509
Jack Collins <BlueSkyFlyer2@gmail.com>
2016-03-05 16:57:56 -0700

    Added Google Map to home page and cleaned up the nav bar

--- home/home.js
+++ home/home.js
@@ -15,4 +15,36 @@ statelineApp.controller('HomeController', ['$scope', function($scope) {
     {name: 'Nutritional and Herbal Medicine'},
     {name: 'Se Habla Espanol'}
   ]
+
+  $scope.services = [
+    {name: 'Sports Physicals'},
+    {name: 'Graston'},
+    {name: 'Decompression'},
+    {name: 'Cold Laser'},
+    {name: 'Ultrasound'},
+    {name: 'DOT Physicals'},
+    {name: 'Same Day X-Rays'},
+    {name: 'Orthodics'}
+  ]
+
+  function initializeMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center:new google.maps.LatLng(45.9929026,-118.38734540000002),
+      zoom: 11
+    });
+    var marker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(45.9929026,-118.38734540000002)
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content:'<strong>Stateline Chiropractic</strong><br>85458 Oregon 11, Milton-Freewater, OR 97862<br>'
+    });
+    google.maps.event.addListener(marker, 'click', function(){
+        infowindow.open(map,marker);
+      });
+    infowindow.open(map,marker);
+  }
+
+  initializeMap()
 }]);
