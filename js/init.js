3af36e84aacae759a6713aab6ce3f6970e1cf926
Jack Collins <BlueSkyFlyer2@gmail.com>
2016-03-10 21:19:45 -0700

    Working on mobile view

--- js/init.js
+++ js/init.js
@@ -1,7 +1,11 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
     $('.slider').slider();
 
   }); // end of document ready
